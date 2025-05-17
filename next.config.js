290ff1adca4a6203f47bb3a0aeac526ea9e9db37
Heikal <heikal.iskandar89@gmail.com>
2025-05-17 22:51:59 +0800

    Fix image layout and deploy-ready code

--- next.config.js
+++ next.config.js
@@ -2,7 +2,14 @@
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+    /* config options here */
+};
+
+module.exports = nextConfig;
+// next.config.js
+const nextConfig = {
+    basePath: "/testing-nextjs",
+    assetPrefix: "/testing-nextjs",
 };
 
 module.exports = nextConfig;
