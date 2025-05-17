a2e6938e155bb9d9b87f0465bff76035a8de7f7a
Heikal <heikal.iskandar89@gmail.com>
2025-05-17 22:52:40 +0800

    Fix asset path for GitHub Pages

--- next.config.js
+++ next.config.js
@@ -1,11 +1,3 @@
-// @ts-check
-
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    /* config options here */
-};
-
-module.exports = nextConfig;
 // next.config.js
 const nextConfig = {
     basePath: "/testing-nextjs",
